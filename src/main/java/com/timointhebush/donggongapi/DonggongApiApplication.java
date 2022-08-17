6206cedc20ea80b93cbd814679f63a46c94fffb2
이준석 <jslee02@miridih.com>
2022-08-17 20:50:46 +0900

    first commit

---
+++ src/main/java/com/timointhebush/donggongapi/DonggongApiApplication.java
@@ -0,0 +1,13 @@
+package com.timointhebush.donggongapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonggongApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonggongApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timointhebush/donggongapi/DonggongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timointhebush.donggongapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonggongApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
